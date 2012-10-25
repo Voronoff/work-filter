8c07e51b27b8b80ed9cbe5b22eac4e7815d69823
David Allen <davidrallen1@gmail.com>
2012-10-24 18:59:56 -0700

    Minor syntax cleaning.

--- workfilter.rb
+++ workfilter.rb
@@ -2,25 +2,25 @@ mode = ARGV[0]
 sites_list = File.open("./sites_list.txt", 'r')
 
 if mode == "on" || mode.nil?
-  hosts = File.open("/etc/hosts", 'a')
+  File.open("/etc/hosts", 'a') do |hosts|
   sites_list.each {|site| hosts.puts("127.0.0.1	" + site)}
-  hosts.close
+  end
 elsif mode == "off"
   lines_to_keep = []
-  hosts = File.open("/etc/hosts", 'r')
+  File.open("/etc/hosts", 'r') do |hosts|
     hosts.each do |line| 
       delete_line = false
-      sites_list.each do |site|
-        delete_line = true if line[site]
-      end
+      sites_list.each {|site| delete_line = true if line[site]}
       lines_to_keep << line unless delete_line
       sites_list.rewind
     end
-  hosts.close
+  end
+
   File.open("/etc/hosts", 'w') do |hosts|
     lines_to_keep.each {|line| hosts.puts(line)}
   end
 else
   puts "Invalid mode. Please speciify on or off. If no mode is given, workfilter will be turned on."
 end
+
 sites_list.close
