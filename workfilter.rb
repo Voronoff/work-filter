b37ff5ae7dd15c41c8d743d8141194f424b7f621
David Allen <davidrallen1@gmail.com>
2012-10-24 18:55:58 -0700

    Now takes in mode as an argument. If on, same as before. If off, removes all the sites in sites_list.txt from /etc/hosts

--- workfilter.rb
+++ workfilter.rb
@@ -1,7 +1,26 @@
-hosts = File.open('/etc/hosts', 'a')
-sites_list = File.open('./sites_list.txt', 'r')
+mode = ARGV[0]
+sites_list = File.open("./sites_list.txt", 'r')
 
-sites_list.each {|site| hosts.puts("127.0.0.1	" + site)}
-
+if mode == "on" || mode.nil?
+  hosts = File.open("/etc/hosts", 'a')
+  sites_list.each {|site| hosts.puts("127.0.0.1	" + site)}
+  hosts.close
+elsif mode == "off"
+  lines_to_keep = []
+  hosts = File.open("/etc/hosts", 'r')
+    hosts.each do |line| 
+      delete_line = false
+      sites_list.each do |site|
+        delete_line = true if line[site]
+      end
+      lines_to_keep << line unless delete_line
+      sites_list.rewind
+    end
+  hosts.close
+  File.open("/etc/hosts", 'w') do |hosts|
+    lines_to_keep.each {|line| hosts.puts(line)}
+  end
+else
+  puts "Invalid mode. Please speciify on or off. If no mode is given, workfilter will be turned on."
+end
 sites_list.close
-hosts.close
